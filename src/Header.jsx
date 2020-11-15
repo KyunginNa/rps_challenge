eefdf4b40cd7eac21811b7314cc08bd9d7c5dd8e
Kyungin Na <KyunginNa@1.local>
2020-11-15 22:35:25 +0100

    Created a header and a footer component, added them in App component.

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Menu } from 'semantic-ui-react'
+
+const Header = () => {
+    return (
+            <Menu color="grey" size="large">
+                <Menu.Item
+                    name='Rock Paper Scissors'
+                />
+            </Menu>
+    )
+}
+
+export default Header
