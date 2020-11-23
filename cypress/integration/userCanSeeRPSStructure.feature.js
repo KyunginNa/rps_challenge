71f731a9a5731bdd131b52bc72e44e6f8acbbcdc
Kyungin Na <KyunginNa@1.local>
2020-11-23 23:30:46 +0100

    Changed the version in footer to ver 3.0

--- cypress/integration/userCanSeeRPSStructure.feature.js
+++ cypress/integration/userCanSeeRPSStructure.feature.js
@@ -2,7 +2,7 @@ describe("RPS interface", () => {
     it("successfully renders", () => {
         cy.visit("http://localhost:3000")
         cy.get("#header").should("contain", "Rock Paper Scissors")
-        cy.get("#footer").should("contain", "Version 2.0")
+        cy.get("#footer").should("contain", "Version 3.0")
         cy.get("#player-header").should("contain", "Player1")
         cy.get("#player2-header").should("contain", "Player2")
     })
