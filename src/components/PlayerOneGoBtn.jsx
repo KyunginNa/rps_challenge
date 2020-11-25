0cff8b05b9af99f0fb00e222b1a356eca7c1cb53
Kyungin Na <KyunginNa@1.local>
2020-11-26 00:52:36 +0100

    Modifying GoBtn components to make it visible always but abled only when user has already clicked one of r/p/s.

--- src/components/PlayerOneGoBtn.jsx
+++ src/components/PlayerOneGoBtn.jsx
@@ -4,21 +4,18 @@ import { Button } from 'semantic-ui-react'
 const PlayerOneGoBtn = ({ playerOnePick, playerOneGoBtn, onClickHandler }) => {
     return (
         <>
-            { playerOnePick && (
-                <Button
-                    disabled={playerOneGoBtn === true ? true : false}
-                    id="player-go"
-                    inverted color='white'
-                    size="big"
-                    onClick={onClickHandler}
-                    style={{
-                        fontFamily: "monospace",
-                        foniSize: 19
-                    }}>
-                    <Button.Content>Go!</Button.Content>
-                </Button>
-            )
-            }
+            <Button
+                disabled={playerOnePick && !playerOneGoBtn ? false : true}
+                id="player-go"
+                inverted color='white'
+                size="big"
+                onClick={onClickHandler}
+                style={{
+                    fontFamily: "monospace",
+                    foniSize: 19
+                }}>
+                <Button.Content>Go!</Button.Content>
+            </Button>
         </>
     )
 }
--- src/components/PlayerTwoGoBtn.jsx
+++ src/components/PlayerTwoGoBtn.jsx
@@ -4,21 +4,18 @@ import { Button } from 'semantic-ui-react'
 const PlayerTwoGoBtn = ({ playerTwoPick, playerTwoGoBtn, onClickHandler }) => {
     return (
         <>
-            {playerTwoPick && (
-                <Button
-                    disabled={playerTwoGoBtn === true ? true : false}
-                    id="player2-go"
-                    inverted color='white'
-                    size="big"
-                    onClick={onClickHandler}
-                    style={{
-                        fontFamily: "monospace",
-                        foniSize: 19
-                    }}>
-                    <Button.Content>Go!</Button.Content>
-                </Button>
-            )
-            }
+            <Button
+                disabled={playerTwoPick && !playerTwoGoBtn ? false : true}
+                id="player2-go"
+                inverted color='white'
+                size="big"
+                onClick={onClickHandler}
+                style={{
+                    fontFamily: "monospace",
+                    foniSize: 19
+                }}>
+                <Button.Content>Go!</Button.Content>
+            </Button>
         </>
     )
 }
