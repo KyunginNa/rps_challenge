cce3b8ad75e14da1ae6d2ee95e01514ff948afff
Kyungin Na <nakyungin93@gmail.com>
2020-12-18 22:24:26 +0100

    Creating rootReducer, configureStore, and initialState in state folder Wrapping App component with Provider module in index.js

---
+++ src/state/store/initialState.js
@@ -0,0 +1,12 @@
+const initialState = {
+  playerOnePick: "",
+  playerOneGoBtn: false,
+  playerOneVisible: true,
+  playerTwoPick: "",
+  playerTwoGoBtn: false,
+  resultMessage: "",
+  playerOneScore: 0,
+  playerTwoScore: 0,
+};
+
+export default initialState;
